636e66e3823f3d19c781ab4c080aa30e3604f3f7
Perni Bharath Raghavendra <pernibharath15@gmail.com>
2025-11-29 12:51:13 +0530

    Delete lib directory

--- lib/mock/mock_profile.dart
+++
@@ -1,9 +0,0 @@
-import '../models/patient_model.dart';
-
-final mockProfile = PatientModel(
-  name: 'Bharath P',
-  email: 'bharath@example.com',
-  phone: '+91 98765 43210',
-  bloodGroup: 'B+',
-  emergencyContact: '+91 98765 00000',
-);
